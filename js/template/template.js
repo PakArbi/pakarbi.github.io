4a50fe6e6acbf9de70096b9a34dfedb08c416dad
Farhan Rizki <farhanrizki101010@gmail.com>
2023-11-06 00:25:41 +0700

    add file

---
+++ js/template/template.js
@@ -0,0 +1,7 @@
+// export let URLPost = `https://asia-southeast2-pasabar.cloudfunctions.net/regispasabar`
+export let URLLogin = `https://asia-southeast2-project3-403614.cloudfunctions.net/Login`
+export let token = 'token';
+
+
+
+
