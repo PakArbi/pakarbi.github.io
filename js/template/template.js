61dfa4172a4d5b78fb4e4629bce28c83b607b0f5
Farhan Rizki <farhanrizki101010@gmail.com>
2023-11-08 01:53:23 +0700

    add

--- js/template/template.js
+++ js/template/template.js
@@ -1,3 +1,3 @@
-// export let URLPost = `https://asia-southeast2-pasabar.cloudfunctions.net/regispasabar`
+export let URLPost = `https://asia-southeast2-lofty-mark-401904.cloudfunctions.net/registerpakarbi`
 export let URLLogin = `https://asia-southeast2-lofty-mark-401904.cloudfunctions.net/loginpakarbi`
 export let token = 'token';
