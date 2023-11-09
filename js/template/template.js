88ff361a2b3a63771d7ee6f461fa62a4cbb72d83
Farhan Rizki <farhanrizki101010@gmail.com>
2023-11-09 14:43:21 +0700

    update template

--- js/template/template.js
+++ js/template/template.js
@@ -1,3 +1,4 @@
+// template js
 export let URLPost = `https://asia-southeast2-lofty-mark-401904.cloudfunctions.net/registerpakarbi`
 export let URLLogin = `https://asia-southeast2-lofty-mark-401904.cloudfunctions.net/loginpakarbi`
 export let token = 'token';
