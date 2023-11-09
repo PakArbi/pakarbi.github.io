3b920012e807dc8ec62b9d5d924167aad422025f
Farhan Rizki <farhanrizki101010@gmail.com>
2023-11-10 00:39:19 +0700

    update controller register

--- js/controller/controllerRegister.js
+++ js/controller/controllerRegister.js
@@ -1,6 +1,14 @@
-import {postWithBearer} from "https://jscroot.github.io/api/croot.js";
-import {GetDataForm,ResponsePost} from "../config/config.js";
-import {token,URLPost} from "../template/template.js";
+import {
+    postWithBearer
+} from "https://jscroot.github.io/api/croot.js";
+import {
+    GetDataForm,
+    ResponsePost
+} from "../config/config.js";
+import {
+    token,
+    URLPost
+} from "../template/template.js";
 
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.querySelector("form");
