8da1990b385cf19567550bd487324b15ba428611
Farhan Rizki <farhanrizki101010@gmail.com>
2023-11-09 22:15:36 +0700

    update template

--- js/template/template.js
+++ js/template/template.js
@@ -1,4 +1,10 @@
 // template js
+
+//url register
 export let URLPost = `https://asia-southeast2-lofty-mark-401904.cloudfunctions.net/registerpakarbi`
+
+//url login 
 export let URLLogin = `https://asia-southeast2-lofty-mark-401904.cloudfunctions.net/loginpakarbi`
+
+//token
 export let token = 'token';
